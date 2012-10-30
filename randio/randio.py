53bf3cc63498e69962610b36a68abc0c55bc26c6
Michel Pelletier <pelletier.michel@gmail.com>
2012-10-29 17:47:07 -0700

    Trying block samples.

--- randio/randio.py
+++ randio/randio.py
@@ -16,64 +16,96 @@ class Randio(Random):
     def __init__(self,
                  dev_index=None,
                  freq_range=None,
-                 freq_count=64,
-                 sample_rate=1.2e6,
-                 gain='auto'):
+                 freq_count=None,
+                 sample_rate=None,
+                 gain=None):
 
+        eget = os.environ.get
         if dev_index is None:
-            dev_index = int(os.environ.get('RANDIO_DEV_INDEX', 0))
+            dev_index = int(eget('RANDIO_DEV_INDEX', 0))
 
         if freq_range is None:
-            low = int(os.environ.get('RANDIO_FREQ_LOW', 64)) * MHZ
-            high = int(os.environ.get('RANDIO_FREQ_HIGH', 1100)) * MHZ
+            low = int(eget('RANDIO_FREQ_LOW', 64)) * MHZ
+            high = int(eget('RANDIO_FREQ_HIGH', 1100)) * MHZ
             freq_range = (low, high, 1000)
 
-        # initialize the pool with some system randomness
+        if freq_count is None:
+            freq_count = int(eget('RANDIO_FREQ_COUNT', 64))
+
+        if sample_rate is None:
+            sample_rate = float(eget('RANDIO_SAMPLE_RATE', 1.2)) * MHZ
+
+        if gain is None and 'RANDIO_GAIN' in os.environ:
+            gain = int(eget('RANDIO_GAIN'))
+        else:
+            gain = 'auto'
+
         self.pool_size = freq_count * SHA_SIZE
         self.pool = bytearray(" " * self.pool_size)
-        self.poolbytes = range(0, self.pool_size, 8)
-        view = memoryview(self.pool)
-        for i in xrange(0, self.pool_size, SHA_SIZE):
-            randbits = hex(self.getrandbits(SHA_SIZE))
-            view[i:i+SHA_SIZE] = sha512(randbits).digest()
-
+        self.poolblocks = range(0, self.pool_size, SHA_SIZE)
         self.radio = rtlsdr.RtlSdr(dev_index)
         self.radio.rs = sample_rate
         self.radio.gain = gain
         self.freq_range = freq_range
         self.freq_count = freq_count
+        self.sample()
         super(Randio, self).__init__()
 
+    def _sample_block(self, start, view):
+        """Take a block offset, resample the block.
+
+        Hashes the block, then updates the hash with radio data of
+        SAMPLE_BLOCK_SIZE from a random frequency in the freq_range.
+        The updated hash is then written back to the block.
+        """
+        end = start + SHA_SIZE
+        frompool = sha512(view[start:end].tobytes())
+        rrange = randrange(*self.freq_range)
+        self.radio.fc = rrange
+        fromradio = self.radio.read_bytes(SAMPLE_BLOCK_SIZE)
+        frompool.update(fromradio)
+        view[start:end] = frompool.digest()
+
     def sample(self):
-        """ Sample some radio data, stirring it into the pool. """
+        """ Sample some radio data over all blocks, stirring the data
+        into the pool.
+
+        This shuffles the pool block order and resamples them in
+        shuffled order.  Every block has a random frequencies worth of
+        radio data hashed into it.
+        """
         view = memoryview(self.pool)
-        ranges = range(0, self.pool_size, SHA_SIZE)
-        shuffle(ranges)
-
-        for start in ranges:
-            rrange = randrange(*self.freq_range)
-            self.radio.fc = rrange
-            end = start + SHA_SIZE
-
-            # get bytes from pool, hash again
-            # read in radio data, more, better
-            # update hash with radio data
-            # save new hash back to pool
-            frompool = sha512(view[start:end].tobytes())
-            fromradio = self.radio.read_bytes(SAMPLE_BLOCK_SIZE)
-            frompool.update(fromradio)
-            view[start:end] = frompool.digest()
+        blocks = self.poolblocks[:]
+        shuffle(blocks)
+        for start in blocks:
+            self._sample_block(start, view)
 
     def random(self):
-        # take a random int from the pool, seed with it
-        i = choice(self.poolbytes)
+        """Takes a random block from the pool, casts it to a long,
+        then seeds the RNG with it.  The block is then resampled.
+        """
+        blocks = self.poolblocks[:]
+        block = choice(blocks)
         view = memoryview(self.pool)
-        stuff = view[i:i+8].tobytes()
+        stuff = view[block:block+SHA_SIZE].tobytes()
         a = long(hexlify(stuff), 16)
         self.seed(a)
+        self._sample_block(block, view)
         return super(Randio, self).random()
 
     def sha512(self):
-        self.sample()
-        return sha512(memoryview(self.pool).tobytes())
+        """Return a sha512 hash of a random block in the pool, then
+        resample that block.
+        """
+        blocks = self.poolblocks[:]
+        block = choice(blocks)
+        view = memoryview(self.pool)
+        result = sha512(view[block:block+SHA_SIZE].tobytes())
+        self._sample_block(block, view)
+        return result
 
+
+class AsyncRandio(Randio):
+
+    def __init__(self, interval, *args, **kwarg):
+        super(AsyncRandio, self).__init__(*args, **kwargs):
